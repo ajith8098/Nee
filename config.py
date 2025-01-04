522792858c290bc046af553be1417a0011587582
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-01-04 06:30:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Bot Token from @BotFather
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "6750957059:AAFvd3mez21vumWuV8jMaG3aOBW55_eGTKM"
 
 # ID of the Telegram channel to monitor (e.g., -1001234567890)
-CHANNEL_ID = -1001234567890
+CHANNEL_ID = -1001870015374
 
 # ID of the logs channel to send ban details (e.g., -1009876543210)
-LOGS_CHANNEL_ID = -1009876543210
+LOGS_CHANNEL_ID = -1001870015374
 
 # Bot Owner's Telegram ID (optional, for direct notifications)
-OWNER_ID = 123456789
+OWNER_ID = 5821871362
 
 # Path to save the ban list
 BAN_LIST_FILE = "ban_list.txt"
