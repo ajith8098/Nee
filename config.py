ac9a3c5b811776fe62496a12dbe61037ea60c03c
ajith8098 <134246822+ajith8098@users.noreply.github.com>
2025-01-04 06:57:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = 23990433  # Replace with your API ID
 API_HASH = "e6c4b6ee1933711bc4da9d7d17e1eb20"  # Replace with your API Hash
 
 # ID of the Telegram channel to monitor (e.g., -1001234567890)
-CHANNEL_ID = -1001870015374
+CHANNEL_ID = -1002051700482
 
 # ID of the logs channel to send ban details (e.g., -1009876543210)
 LOGS_CHANNEL_ID = -1001870015374
